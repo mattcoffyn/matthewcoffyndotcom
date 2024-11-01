a2a2beaabdc7834381a21e61cf6092b8110340ff
Matt Coffyn <matt.coffyn@gmail.com>
2024-11-01 15:15:11 +0000

    more filters & about page started

---
+++ app/about/page.js
@@ -0,0 +1,45 @@
+import localFont from 'next/font/local';
+import Image from 'next/image';
+import styles from '../styles/about.module.css';
+import profilePhoto from '../../public/profilePhoto.png';
+
+const trats = localFont({
+  src: '../fonts/TRATS.woff2',
+  display: 'swap',
+});
+
+const sans = localFont({
+  src: '../fonts/SwedenSansBook.woff2',
+  display: 'swap',
+});
+
+export default function About() {
+  return (
+    <section className={`${styles.aboutGrid} ${trats.className}`}>
+      <h1 className={styles.title}>About</h1>
+      <Image
+        alt="A picture of Matt looking kinda cool but admittedly quite moody."
+        src={profilePhoto}
+        width={700}
+        height={475}
+        sizes="100vw"
+        style={{
+          width: '100%',
+          height: 'auto',
+        }}
+      />
+      <div className={`${styles.bio} ${sans.className}`}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus
+        velit ea quis nulla possimus laboriosam laudantium ratione inventore rem
+        perferendis voluptate eveniet, repellat delectus amet natus mollitia
+        blanditiis maxime quos!
+        <br />
+        <br />
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente autem
+        sit sint odio eaque nulla obcaecati architecto! Recusandae mollitia
+        explicabo amet ullam consequatur consectetur, modi quas fuga, placeat
+        maiores deleniti!
+      </div>
+    </section>
+  );
+}
--- app/components/Background.js
+++ app/components/Background.js
@@ -25,21 +25,21 @@ export default function Background() {
       />
     );
   }
-  if (pathname == '/about') {
-    return (
-      <Image
-        className={styles.fadeIn}
-        alt="Black & white image of Lynchburg, Tennessee"
-        src={lynchburg}
-        placeholder="blur"
-        quality={100}
-        fill={true}
-        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-        style={{
-          objectFit: 'cover',
-          zIndex: 0,
-        }}
-      />
-    );
-  }
+  // if (pathname == '/about') {
+  //   return (
+  //     <Image
+  //       className={styles.fadeIn}
+  //       alt="Black & white image of Lynchburg, Tennessee"
+  //       src={lynchburg}
+  //       placeholder="blur"
+  //       quality={100}
+  //       fill={true}
+  //       sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+  //       style={{
+  //         objectFit: 'cover',
+  //         zIndex: 0,
+  //       }}
+  //     />
+  //   );
+  // }
 }
