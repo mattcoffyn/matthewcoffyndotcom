c2facaf8d1de5aeaa2695d26713b7adb282953c6
Matt Coffyn <matt.coffyn@gmail.com>
2024-11-02 00:13:18 +0000

    progress...

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,8 +1,9 @@
 'use client';
 
-import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { usePathname } from 'next/navigation';
 import localFont from 'next/font/local';
-import { useState } from 'react';
+import BurnLink from './BurnLink';
 import styles from '../styles/nav.module.css';
 
 const tratv = localFont({
@@ -15,47 +16,25 @@ const trats = localFont({
 });
 
 export default function Nav() {
-  const [burn, setBurn] = useState(0);
-
-  const burnOut = (e) => {
-    // e.target.classList.add(`${styles.burnout}`);
-    setBurn(1);
-    // setTimeout(() => {
-    //   setBurn(0);
-    // }, 1000);
-  };
-
-  const resetBurn = (e) => {
-    setBurn(0);
-    console.log('Mouse Enter');
-    // e.target.classList.remove(`${styles.burnout}`);
-  };
-
   return (
-    <aside
-      className={styles.navContainer}
-      onMouseEnter={(e) => {
-        resetBurn(e);
-      }}
-    >
-      <ul className={`${styles.nav} ${trats.className}`}>
-        <li className={`${styles.navItem} `}>
-          <Link href="/">Home</Link>
-        </li>
-        <li
-          className={`${styles.navItem} ${burn ? styles.burnout : ''} `}
-          onClick={(e) => {
-            burnOut(e);
-          }}
-        >
-          <Link href="/about">About</Link>
-        </li>
-        <li className={`${styles.navItem} `}>
-          <Link href="/listen">Listen</Link>
-        </li>
-        <li className={`${styles.navItem} `}>
-          <Link href="/contact">Contact</Link>
-        </li>
+    <aside className={`${styles.navContainer} ${trats.className}`}>
+      <ul className={styles.nav}>
+        <BurnLink
+          text="Home"
+          slug="/"
+        />
+        <BurnLink
+          text="About"
+          slug="/about"
+        />
+        <BurnLink
+          text="Listen"
+          slug="/listen"
+        />
+        <BurnLink
+          text="Contact"
+          slug="/contact"
+        />
       </ul>
     </aside>
   );
---
+++ app/contact/page.js
@@ -0,0 +1,79 @@
+import localFont from 'next/font/local';
+import styles from '../styles/contact.module.css';
+
+const trats = localFont({
+  src: '../fonts/TRATS.woff2',
+  display: 'swap',
+});
+
+const sans = localFont({
+  src: '../fonts/SwedenSansBook.woff2',
+  display: 'swap',
+});
+
+export default function Contact() {
+  return (
+    <section className={`${styles.contactGrid} ${trats.className}`}>
+      <h1 className={styles.title}>Contact</h1>
+      <div className={`${styles.email} ${sans.className}`}>
+        <h2>Email</h2>
+        <p>
+          If you want to discuss a project or just have a chat (I'd love that),
+          drop me an email and I'll get back to you ASAP
+        </p>
+        <a
+          href="mailto:hello@matthewcoffyn.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          hello@matthewcoffyn.com
+        </a>
+      </div>
+      <div className={`${styles.links} ${sans.className}`}>
+        <h2>Socials</h2>
+        <p>
+          You can also find me on socials so feel free to reach out or follow me
+          here.
+        </p>
+        <ul>
+          <li>
+            <a
+              href="mailto:hello@matthewcoffyn.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              instagram
+            </a>
+          </li>
+          <li>
+            <a
+              href="mailto:hello@matthewcoffyn.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              facebook
+            </a>
+          </li>
+          <li>
+            <a
+              href="mailto:hello@matthewcoffyn.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              twitter
+            </a>
+          </li>
+          <li>
+            <a
+              href="mailto:hello@matthewcoffyn.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              imdb
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+}
